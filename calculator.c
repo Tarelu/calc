28992b02250799177f350755e20ba83d973993ee
Tarelu <94992130+Tarelu@users.noreply.github.com>
2024-10-24 10:57:18 +0300

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -9,7 +9,7 @@ double calculate(double num1, double num2, char operation) {
     double result;
     switch (operation) {
         case '+':
-            result = num1 + num2+1;
+            result = num1 + num2;
             break;
         case '-':
             result = num1 - num2;
@@ -31,7 +31,3 @@ double calculate(double num1, double num2, char operation) {
     return result;
 }
 
-int func (){
-    int a[5];
-    a[6]=0;
-}
